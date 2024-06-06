1668721fb998813066ecb95f429fd6f0d9d67986
julio-1610 <jpaucarc@unsa.edu.pe>
2024-06-06 14:25:25 -0500

    Código limpio, eliminación de algunas funciones y variables irrelevantes o redundantes

--- app/src/main/java/com/idnp2024a/beaconscanner/MainActivity.kt
+++ app/src/main/java/com/idnp2024a/beaconscanner/MainActivity.kt
@@ -6,88 +6,66 @@ import android.bluetooth.BluetoothManager
 import android.bluetooth.le.BluetoothLeScanner
 import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanResult
-import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
-    val PERMISSION_REQUEST_COARSE_LOCATION = 1000
-    val REQUEST_ENABLE_BT = 100
-    var btManager: BluetoothManager? = null;
-    var btScanner: BluetoothLeScanner? = null
+    private val PERMISSION_REQUEST_COARSE_LOCATION = 1000
+    private var btManager: BluetoothManager? = null
+    private var btScanner: BluetoothLeScanner? = null
 
     private var btPermission = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        /*       ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-                   val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-                   v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-                   insets
-               }*/
 
-        //setUpBluetoothManager()
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.BLUETOOTH_SCAN
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return
-        }
-
-        //checkForLocationPermission()
         setUpBluetoothManager()
 
-        val button = findViewById<Button>(R.id.button)
-        button.setOnClickListener {
-            btScanner!!.startScan(leScanCallback)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.BLUETOOTH_SCAN
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return@setOnClickListener
+            }
+            btScanner?.startScan(leScanCallback)
         }
-
-
     }
 
     private fun checkForLocationPermission() {
-        // Make sure we have access coarse location enabled, if not, prompt the user to enable it
         if (ContextCompat.checkSelfPermission(
                 applicationContext,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION
+                Manifest.permission.ACCESS_COARSE_LOCATION
             ) != PackageManager.PERMISSION_GRANTED
         ) {
-            val builder = AlertDialog.Builder(applicationContext)
+            val builder = AlertDialog.Builder(this)
             builder.setTitle("This app needs location access")
-            builder.setMessage("Please grant location access so this app can detect  peripherals.")
+            builder.setMessage("Please grant location access so this app can detect peripherals.")
             builder.setPositiveButton(android.R.string.ok, null)
             builder.setOnDismissListener {
                 requestPermissions(
-                    arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION),
+                    arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
                     PERMISSION_REQUEST_COARSE_LOCATION
                 )
-                /*                requestPermissions(
-                                    arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
-                                    PERMISSION_REQUEST_COARSE_LOCATION
-                                )*/
             }
             builder.show()
         }
@@ -98,79 +76,49 @@ class MainActivity : AppCompatActivity() {
         permissions: Array<String>, grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            PERMISSION_REQUEST_COARSE_LOCATION -> {
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    println("coarse location permission granted")
-                } else {
-                    val builder = AlertDialog.Builder(applicationContext)
-                    builder.setTitle("Functionality limited")
-                    builder.setMessage("Since location access has not been granted, this app will not be able to discover BLE beacons")
-                    builder.setPositiveButton(android.R.string.ok, null)
-                    builder.setOnDismissListener { }
-                    builder.show()
-                }
-                return
+        if (requestCode == PERMISSION_REQUEST_COARSE_LOCATION) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                println("coarse location permission granted")
+            } else {
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("Functionality limited")
+                builder.setMessage("Since location access has not been granted, this app will not be able to discover BLE beacons")
+                builder.setPositiveButton(android.R.string.ok, null)
+                builder.show()
             }
         }
     }
 
     private fun setUpBluetoothManager() {
-        //btManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
-        btManager = getSystemService(BluetoothManager::class.java) as BluetoothManager
-        val btAdapter = btManager!!.adapter
+        btManager = getSystemService(BluetoothManager::class.java)
+        val btAdapter = btManager?.adapter
         btScanner = btAdapter?.bluetoothLeScanner
-        Log.d("MainActivity", "setUpBluetoothManager 1")
-        if (btAdapter != null && !btAdapter!!.isEnabled) {
-            Log.d("MainActivity", "setUpBluetoothManager 2")
+
+        if (btAdapter != null && !btAdapter.isEnabled) {
             val enableIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
             btActivityResultLauncher.launch(enableIntent)
-            //startActivityForResult(enableIntent, REQUEST_ENABLE_BT)
         }
-
 
         checkForLocationPermission()
     }
 
     private val leScanCallback: ScanCallback = object : ScanCallback() {
         override fun onScanResult(callbackType: Int, result: ScanResult) {
-            //Result will be retrieved here
-
-            Log.d("MainActivity", result.rssi.toString())
+            Log.d("MainActivity", "RSSI: ${result.rssi}")
             if (ActivityCompat.checkSelfPermission(
                     applicationContext,
                     Manifest.permission.BLUETOOTH_CONNECT
                 ) != PackageManager.PERMISSION_GRANTED
             ) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
                 return
             }
-            Log.d("MainActivity", "----------------")
-            Log.d("MainActivity", result.device.name.toString())
+            Log.d("MainActivity", "Device: ${result.device.name}")
         }
 
         override fun onScanFailed(errorCode: Int) {
-            Log.d("MainActivity", "Error" + errorCode.toString())
+            Log.d("MainActivity", "Scan failed with error: $errorCode")
         }
     }
-
-    /*    private fun onStartScannerButtonClick() {
-            startButton.visibility = View.GONE
-            stopButton.visibility = View.VISIBLE
-            btScanner!!.startScan(leScanCallback)
-        }
-
-        private fun onStopScannerButtonClick() {
-            stopButton.visibility = View.GONE
-            startButton.visibility = View.VISIBLE
-            btScanner!!.stopScan(leScanCallback)
-        }*/
 
     private val blueToothPermissionLauncher = registerForActivityResult(
         ActivityResultContracts.RequestPermission()
@@ -199,7 +147,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun btScan() = Toast.makeText(this, "BT接続できます", Toast.LENGTH_LONG).show()
-
-
+    private fun btScan() {
+        Toast.makeText(this, "BT接続できます", Toast.LENGTH_LONG).show()
+    }
 }
--- app/src/main/java/com/idnp2024a/beaconscanner/MainActivityBLE.kt
+++ app/src/main/java/com/idnp2024a/beaconscanner/MainActivityBLE.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.bluetooth.le.BluetoothLeScanner
-import android.bluetooth.le.ScanResult
 import android.content.Intent
 import android.location.LocationManager
 import android.net.Uri
@@ -23,23 +22,20 @@ import com.idnp2024a.beaconscanner.permissions.PermissionManager
 
 class MainActivityBLE : AppCompatActivity() {
 
-    private val TAG: String = "MainActivityBLE"
-    private var alertDialog: AlertDialog? = null //Un cuadro de diálogo para mostrar alertas al usuario.
-    private lateinit var bluetoothManager: BluetoothManager //Administrador de Bluetooth
+    private val TAG = "MainActivityBLE"
+    private lateinit var bluetoothManager: BluetoothManager
     private lateinit var btScanner: BluetoothLeScanner
     private lateinit var bluetoothAdapter: BluetoothAdapter
-    private lateinit var txtMessage: TextView;
-    private val permissionManager = PermissionManager.from(this) //Objeto para gestionar los permisos.
+    private lateinit var txtMessage: TextView
+    private val permissionManager = PermissionManager.from(this)
 
-    private val rssiFilter = MovingAverageFilter(5) // Ventana de tamaño 5
+    private val rssiFilter = MovingAverageFilter(5)
     private val btPermissions = BTPermissions(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //enableEdgeToEdge()
         setContentView(R.layout.activity_main_ble)
 
-        //TPermissions(this).check()
         btPermissions.check()
         initBluetooth()
 
@@ -48,40 +44,34 @@ class MainActivityBLE : AppCompatActivity() {
         val btnStop = findViewById<Button>(R.id.btnStop)
         txtMessage = findViewById(R.id.txtMessage)
 
-
         val bleScanCallback = BleScanCallback(
             onScanResultAction = { result, _ ->
                 result?.let {
-                    val smoothedRSSI = rssiFilter.filter(it.rssi) // USO DEL FILTRO DE MEDIA MÓVIL
-                    Log.d(TAG, "Original RSSI: ${it.rssi}, Smoothed RSSI: $smoothedRSSI") // LOG DEL FILTRO
+                    val smoothedRSSI = rssiFilter.filter(it.rssi)
+                    Log.d(TAG, "Original RSSI: ${it.rssi}, Smoothed RSSI: $smoothedRSSI")
                     val scanRecord = it.scanRecord
                     val beacon = Beacon(it.device.address)
-                    beacon.manufacturer = it.device.name // Asignación del fabricante directamente
-                    beacon.rssi = smoothedRSSI.toInt() // ASIGNACIÓN DEL RSSI FILTRADO
+                    beacon.manufacturer = it.device.name
+                    beacon.rssi = smoothedRSSI.toInt()
                     if (scanRecord != null && beacon.manufacturer == "ESP32 Beacon") {
                         scanRecord.bytes?.let { bytes -> decodeiBeacon(bytes, beacon.rssi) }
                     }
                 }
             },
             onBatchScanResultAction = { results ->
-                if (results != null) {
-                    Log.d(TAG, "BatchScanResult $results")
-                }
+                Log.d(TAG, "BatchScanResult $results")
             },
             onScanFailedAction = { errorCode ->
                 Log.d(TAG, "ScanFailed $errorCode")
             }
         )
 
-
         btnAdversting.setOnClickListener {
-//            val iBeaconEmissor=IBeaconEmissor(applicationContext)
-//            iBeaconEmissor.emissor()
+            // Código comentado
         }
 
         btnStart.setOnClickListener {
             if (isLocationEnabled()) {
-                // Verifica los permisos de ubicación antes de comenzar el escaneo Bluetooth
                 permissionManager
                     .request(Permission.Location)
                     .rationale("Bluetooth scanning requires location access.")
@@ -89,13 +79,10 @@ class MainActivityBLE : AppCompatActivity() {
                         if (isGranted) {
                             bluetoothScanStart(bleScanCallback)
                         } else {
-                            // El usuario no concedió permisos de ubicación, muestra un mensaje o solicita permisos nuevamente.
-                            // Por ejemplo, puedes mostrar un diálogo pidiendo permisos de ubicación.
                             showLocationPermissionDeniedDialog()
                         }
                     }
             } else {
-                // La ubicación está desactivada, muestra un mensaje o solicita al usuario que active la ubicación.
                 showLocationSettingsDialog()
             }
         }
@@ -103,13 +90,11 @@ class MainActivityBLE : AppCompatActivity() {
         btnStop.setOnClickListener {
             bluetoothScanStop(bleScanCallback)
         }
-
     }
 
-    fun initBluetooth() {
-
+    private fun initBluetooth() {
         bluetoothManager = getSystemService(BluetoothManager::class.java)
-        bluetoothAdapter = bluetoothManager!!.adapter
+        bluetoothAdapter = bluetoothManager.adapter
 
         if (bluetoothAdapter != null) {
             btScanner = bluetoothAdapter.bluetoothLeScanner
@@ -119,25 +104,20 @@ class MainActivityBLE : AppCompatActivity() {
     }
 
     private fun bluetoothScanStart(bleScanCallback: BleScanCallback) {
-        Log.d(TAG, "btScan ...1")
         if (btScanner != null) {
-            Log.d(TAG, "btScan ...2")
             permissionManager
                 .request(Permission.Location)
                 .rationale("Bluetooth permission is needed")
-                .checkPermission { isgranted ->
-                    if (isgranted) {
-                        btScanner!!.startScan(bleScanCallback)
+                .checkPermission { isGranted ->
+                    if (isGranted) {
+                        btScanner.startScan(bleScanCallback)
                     } else {
-                        Log.d(TAG, "Alert you don't have Bluetooth permission")
+                        Log.d(TAG, "Alert: you don't have Bluetooth permission")
                     }
-
                 }
-
         } else {
             Log.d(TAG, "btScanner is null")
         }
-
     }
 
     private fun isLocationEnabled(): Boolean {
@@ -145,44 +125,16 @@ class MainActivityBLE : AppCompatActivity() {
         return LocationManagerCompat.isLocationEnabled(locationManager)
     }
 
-
     @SuppressLint("MissingPermission")
     private fun bluetoothScanStop(bleScanCallback: BleScanCallback) {
-        Log.d(TAG, "btScan ...1")
         if (btScanner != null) {
-            Log.d(TAG, "btScan ...2")
-            btScanner!!.stopScan(bleScanCallback)
-
+            btScanner.stopScan(bleScanCallback)
         } else {
             Log.d(TAG, "btScanner is null")
         }
-
     }
 
-    @SuppressLint("MissingPermission")
-    val onScanResultAction: (ScanResult?) -> Unit = { result ->
-        val scanRecord = result?.scanRecord
-        val beacon = Beacon(result?.device?.address)
-        beacon.manufacturer = result?.device?.name
-        beacon.rssi = result?.rssi
-        if (scanRecord != null && beacon.manufacturer == "ESP32 Beacon") {
-            scanRecord?.bytes?.let { decodeiBeacon(it, beacon.rssi) }
-
-        }
-
-    }
-
-    val onBatchScanResultAction: (MutableList<ScanResult>?) -> Unit = {
-        if (it != null) {
-            Log.d(TAG, "BatchScanResult " + it.toString())
-        }
-    }
-
-    val onScanFailedAction: (Int) -> Unit = {
-        Log.d(TAG, "ScanFailed " + it.toString())
-    }
-
-    fun decodeiBeacon(data: ByteArray, rssi: Int?) {
+    private fun decodeiBeacon(data: ByteArray, rssi: Int?) {
         val data_len = Integer.parseInt(Utils.toHexString(data.copyOfRange(0, 1)), 16)
         val data_type = Integer.parseInt(Utils.toHexString(data.copyOfRange(1, 2)), 16)
         val LE_flag = Integer.parseInt(Utils.toHexString(data.copyOfRange(2, 3)), 16)
@@ -196,45 +148,23 @@ class MainActivityBLE : AppCompatActivity() {
         val minor = Integer.parseInt(Utils.toHexString(data.copyOfRange(27, 29)), 16)
         val txPower = Integer.parseInt(Utils.toHexString(data.copyOfRange(29, 30)), 16)
 
-        //var factor = (-40 - rssi!!)/(10*2.0)
-        var factor = (-1 * txPower - rssi!!) / (10 * 4.0)
-        var distance = Math.pow(10.0, factor)
-        //var rssi2= movingAverage.next(rssi)
+        val factor = (-1 * txPower - rssi!!) / (10 * 4.0)
+        val distance = Math.pow(10.0, factor)
 
-        var display = "TxPower:$txPower \nRSSI:$rssi \nRSSI2:rssi2 \nDistance:$distance"
-        txtMessage.setText(display)
+        val display = "TxPower:$txPower \nRSSI:$rssi \nDistance:$distance"
+        txtMessage.text = display
 
         Log.d(
             TAG,
             "DECODE data_len:$data_len data_type:$data_type LE_flag:$LE_flag len:$len type:$type subtype:$subtype subtype_len:$subtypelen company:$company UUID:$iBeaconUUID major:$major minor:$minor txPower:$txPower"
         )
-
-
     }
 
-    private fun showPermissionDialog() {
-
-        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
-        builder.setTitle("Alerta")
-            .setMessage("El servicio de localizacion no esta activo")
-            .setPositiveButton("Close") { dialog, which ->
-                dialog.dismiss()
-            }
-
-        if (alertDialog == null) {
-            alertDialog = builder.create()
-        }
-
-        if (!alertDialog!!.isShowing()) {
-            alertDialog!!.show()
-        }
-    }
     private fun showLocationPermissionDeniedDialog() {
         AlertDialog.Builder(this)
             .setTitle("Location Permission Required")
             .setMessage("This feature requires location permission. Please grant the permission in the app settings.")
             .setPositiveButton("Settings") { _, _ ->
-                // Abre la configuración de la aplicación para que el usuario pueda conceder los permisos manualmente.
                 openAppSettings()
             }
             .setNegativeButton("Cancel") { dialog, _ ->
@@ -248,7 +178,6 @@ class MainActivityBLE : AppCompatActivity() {
             .setTitle("Location Services Disabled")
             .setMessage("Location services need to be enabled for this feature. Do you want to enable them?")
             .setPositiveButton("Settings") { _, _ ->
-                // Abre la configuración del dispositivo para que el usuario pueda habilitar la ubicación manualmente.
                 openLocationSettings()
             }
             .setNegativeButton("Cancel") { dialog, _ ->
@@ -268,5 +197,4 @@ class MainActivityBLE : AppCompatActivity() {
         val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
         startActivity(intent)
     }
-
 }
